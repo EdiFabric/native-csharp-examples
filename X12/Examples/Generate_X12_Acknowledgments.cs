50c27020f8465d43c5fd8db4c56652f345f2070b
Kamen Nikolov <kamen.nikolov@edifabric.com>
2026-06-25 16:47:47 +0100

    Initial upload

---
+++ X12/Examples/Generate_X12_Acknowledgments.cs
@@ -0,0 +1,29 @@
+﻿namespace X12
+{
+    internal partial class Examples
+    {
+        /// <summary>
+        /// Parses, validates and generates a X12 999 ack for a valid X12 file
+        /// Set ParseMode to Ack - this parses and validates the X12 data, AND generates an acknowledgment 
+        /// Pass Ack configuration in parseConfig
+        /// </summary>
+        public static void Ack_valid_x12()
+        {
+            X12Client.SetMap(mapFile);
+            var result = X12Client.Parse(edi837P, ParseMode.Ack, parseConfig);          
+            Console.WriteLine($"         ACK generated ✓");
+        }
+
+        /// <summary>
+        /// Parses, validates and generates a X12 999 ack for an invalid X12 file
+        /// Set ParseMode to Ack - this parses and validates the X12 data, AND generates an acknowledgment 
+        /// Pass Ack configuration in parseConfig
+        /// </summary>
+        public static void Ack_invalid_x12()
+        {
+            X12Client.SetMap(mapFile);
+            var result = X12Client.Parse(edi837PError, ParseMode.Ack, parseConfig);
+            Console.WriteLine($"         ACK with errors generated ✓");
+        }     
+    }
+}
---
+++ X12/X12Config.cs
@@ -0,0 +1,20 @@
+﻿namespace X12
+{
+    internal partial class Examples
+    {
+        public const string serialKey = "bd96a836feca45cb91c86ee65d281f52";
+
+        const string ediDir = "../../../EDI/";
+        const string mapDir = "../../../MAP/";
+        const string jsonDir = "../../../JSON/";
+
+        static byte[] edi837P = File.ReadAllBytes(Path.Combine(ediDir, "837P.txt"));
+        static byte[] edi837PError = File.ReadAllBytes(Path.Combine(ediDir, "837P_CodeError.txt"));
+        static byte[] mapFile = File.ReadAllBytes(Path.Combine(mapDir, "Map.json"));
+        static byte[] mapFileDefault = File.ReadAllBytes(Path.Combine(mapDir, "MapDefault.json"));
+        static byte[] json837P = File.ReadAllBytes(Path.Combine(jsonDir, "837P.json"));
+
+        static string splitConfig = File.ReadAllText(Path.Combine(jsonDir, "SplitConfig.json"));
+        static string parseConfig = File.ReadAllText(Path.Combine(jsonDir, "ParseConfig.json"));
+    }
+}
